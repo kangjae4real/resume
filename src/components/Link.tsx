7446dcf53d6689807d94c49a0330af939c0e9cb5
Kangjae Choi <awpe89@gmail.com>
2024-01-21 15:52:27 +0900

    fix career structure

---
+++ src/components/Link.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { MOBILE_MEDIA_QUERY } from "../styles/mediaQuery";
+
+interface AnchorProps {
+  withUnderline?: boolean;
+}
+
+const Anchor = styled.a<AnchorProps>`
+  position: relative;
+  font-size: 28px;
+  font-weight: bold;
+  color: #000 !important;
+  text-decoration: none;
+
+  ${({ withUnderline }) =>
+    withUnderline &&
+    css`
+      &:after {
+        position: absolute;
+        content: "";
+        width: 100%;
+        height: 4px;
+        background: #000;
+        bottom: -5px;
+        left: 0;
+      }
+    `}
+
+  ${MOBILE_MEDIA_QUERY} {
+    font-size: 32px;
+  }
+`;
+
+interface LinkProps extends AnchorProps {
+  href: string;
+  children: React.ReactNode | string;
+}
+
+const Link: React.FC<LinkProps> = ({ href, children, withUnderline }) => {
+  return (
+    <Anchor href={href} withUnderline={withUnderline} target="_blank">
+      {children}
+    </Anchor>
+  );
+};
+
+export default Link;
--- src/values/career.ts
+++ src/values/career.ts
@@ -1,8 +1,10 @@
 export interface Career {
   id: number;
-  companyName: string;
+  companyNames: string[];
   team: string;
   job: string;
+  particulars?: string;
+  links?: string[];
   startedAt: string;
   endAt: string;
 }
@@ -10,33 +12,21 @@ export interface Career {
 export const CAREER_LIST: Career[] = [
   {
     id: 1,
-    companyName: "제노임펙트",
+    companyNames: ["탱커", "제노임펙트"],
     team: "Techne 1 팀",
     job: "Front-end Developer",
-    startedAt: "2023.09",
+    particulars:
+      "탱커의 경영난으로 기술팀 전원 제노임펙트로 넘어가게 되었습니다.\n소속은 바뀌었지만, 여전히 탱커에서의 업무를 진행하고 있습니다.\n(인수)",
+    links: ["https://tanker.ai", "https://www.xenoimpact.com"],
+    startedAt: "2021.09",
     endAt: "현재",
   },
   {
     id: 2,
-    companyName: "위어",
-    team: "FE Part 팀",
-    job: "Front-end Developer",
-    startedAt: "2022.04",
-    endAt: "현재",
-  },
-  {
-    id: 3,
-    companyName: "탱커",
-    team: "기술 팀",
-    job: "Front-end Developer",
-    startedAt: "2021.09",
-    endAt: "2023.08",
-  },
-  {
-    id: 4,
-    companyName: "팀블랙버드(CryptoQuant)",
+    companyNames: ["팀블랙버드(CryptoQuant)"],
     team: "Live 팀",
     job: "Front-end Developer",
+    links: ["https://cryptoquant.com"],
     startedAt: "2020.09",
     endAt: "2021.08",
   },
@@ -82,74 +72,6 @@ export const CAREER_DETAIL_LIST: CareerDetail[] = [
         startedAt: "2023.09",
         endAt: "현재",
       },
-    ],
-  },
-  {
-    id: 2,
-    detail: [
-      {
-        title: "JYP Partners 사이트 개발",
-        description: "JYP Partners 요청으로 개발한 Next.js 베이스 반응형 웹사이트입니다.",
-        link: "https://jyppartners.com",
-        skills: ["TypeScript", "Next.js", "styled-components", "axios"],
-        work: [
-          "모든 페이지 SSR로 동작하도록 개발",
-          "모든 페이지 다국어화 (영문, 한글)",
-          "CSS 애니메이션 개발",
-          "FE, BE 애플리케이션을 Dockerizing하여 배포",
-        ],
-        improve: [
-          "검색엔진최적화(SEO) 진행",
-          "Open API Generator 도입으로 생성된 요청코드만\n사용하도록 하여 개발자경험을 개선",
-        ],
-        startedAt: "2023.09",
-        endAt: "2023.10",
-      },
-      {
-        title: "On-Premise Issue Tracker 개발",
-        description:
-          "RiskX 요청으로 개발한 React 베이스 반응형 웹사이트입니다.\nJira/Bitbucket Relay API를 활용합니다.",
-        skills: ["TypeScript", "React", "styled-components", "axios"],
-        work: [
-          "이슈를 생성하고 조회하며 핸들링하는 페이지 개발",
-          "브랜치를 생성하고 조회하는 페이지 개발",
-          "커밋 리스트를 조회하며 댓글을 달 수 있는 기능이 포함된 페이지 개발",
-          "풀 리퀘스트를 생성하고 조회하는 페이지 개발",
-          "저장소 다운로드 기능 개발",
-        ],
-        improve: [
-          "Open API Generator 도입으로 생성된 요청코드만\n사용하도록 하여 개발경험을 개선",
-          "불필요한 API 요청코드를 제거하여 페이지 로딩속도\n기존 10초에서 4초로 단축",
-          "기존 Atlassian API 구조 상 요청의 불필요한 베이스 코드가 필요,\n이를 새로이 개발된 Relay API를 사용하도록 모두 변경하여 로딩 개선",
-          "Atlassian API 댓글 Endpoint는 그 응답이 Front-end에서\n사용하기 굉장히 어려운 상태였습니다.\n이를, 사용하기 좋게 포맷팅 해주는 스크립트를 개발하여\n개발자 경험을 개선하였습니다.",
-        ],
-        startedAt: "2023.05",
-        endAt: "2023.08",
-      },
-      {
-        title: "WitU MCN 크리에이터, 관리자 사이트 개발",
-        description:
-          "Witu MCN 요청으로 개발한 Next.js 베이스 웹사이트입니다.\n크리에이터 사이트, 관리자 사이트 2개의 사이트를 개발하였습니다.",
-        skills: ["TypeScript", "Next.js", "styled-components", "axios"],
-        work: [
-          "크리에이터의 캠페인 정산 및 관리하는 페이지 개발",
-          "관리자의 상담등록 및 크리에이터 정산요청을 관리하는 페이지 개발",
-          "관리자의 사내 직원을 관리하는 페이지 개발",
-          "사용자 인증(로그인, 내정보페이지) 기능 개발",
-        ],
-        improve: [
-          "검색엔진최적화(SEO) 진행",
-          "Open API Generator 도입으로 생성된 요청코드만\n사용하도록 하여 개발경험을 개선",
-          "데이터와 필요내용을 담으면 페이지 레이아웃을 렌더링해주는\nLayout 컴포넌트를 개발하여 개발경험을 개선",
-        ],
-        startedAt: "2023.01",
-        endAt: "2023.04",
-      },
-    ],
-  },
-  {
-    id: 3,
-    detail: [
       {
         title: "Doczip",
         description: "유료사용자 50,000명의 서비스로 반응형 웹\n주소지 검색 및 공부 열람 서비스입니다.",
@@ -201,7 +123,7 @@ export const CAREER_DETAIL_LIST: CareerDetail[] = [
     ],
   },
   {
-    id: 4,
+    id: 2,
     detail: [
       {
         title: "CryptoQuant",
